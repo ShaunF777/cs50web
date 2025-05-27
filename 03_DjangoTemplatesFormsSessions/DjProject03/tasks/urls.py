9eab2ba0c93a9b794df84258cdbf48eb83e914d7
ShaunF777 <shaunfr777@gmail.com>
2025-05-27 13:33:56 +0200

    define add function inside views.py and add its path to urlpatterns. Create  /templates/tasks/add.html

--- 03_DjangoTemplatesFormsSessions/DjProject03/tasks/urls.py
+++ 03_DjangoTemplatesFormsSessions/DjProject03/tasks/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 # Define URL patterns where i would like a path to the index view
 urlpatterns = [
+    # "" is an empty path that loads the index function, whos name is index
     path("", views.index, name="index"),
+    # when i goto /tasks/add, it will call the add function inside views.py for rendering
+    path("add", views.add, name="add")
 ]
-# "" is an empty path that loads the index function, whos name is index
--- 03_DjangoTemplatesFormsSessions/DjProject03/tasks/views.py
+++ 03_DjangoTemplatesFormsSessions/DjProject03/tasks/views.py
@@ -4,13 +4,16 @@ tasks = ["eat", "drink", "sleep"] # Global variable that holds a list of tasks
 
 # Create your views functions here. 
 def index(request): # define a function called index that takes a request as an argument
-    '''render a template(page) called "tasks/index.html"
-    and pass the tasks list to the template context '''
-    return render(request, "tasks/index.html", { 
-        "tasks": tasks 
-    })
-    ''' When Django is rendering the template,it will look for 
+    '''Render a template(page) called "tasks/index.html"
+    and pass the tasks list to the template context.
+    When Django is rendering the template,it will look for 
     a variable called "tasks" in the context dictionary.
     "tasks" is the key, that the HTML template has access to and
     tasks is the value or python variable passed into it.
     This allows the template to access the list of tasks and display them.'''
+    return render(request, "tasks/index.html", { 
+        "tasks": tasks 
+    })
+
+def add(request):
+    return render(request, "tasks/add.html")
