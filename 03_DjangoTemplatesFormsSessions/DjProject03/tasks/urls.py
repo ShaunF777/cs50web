4e1b6efb63791f07490b884ff38764081404cb56
ShaunF777 <shaunfr777@gmail.com>
2025-05-27 21:30:57 +0200

    Add app-specific navigation links to add and index task pages, like {% url 'tasks:add' %}

--- 03_DjangoTemplatesFormsSessions/DjProject03/tasks/urls.py
+++ 03_DjangoTemplatesFormsSessions/DjProject03/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"  # namespace for the app, to avoid name clashes with other apps
 # Define URL patterns where i would like a path to the index view
 urlpatterns = [
     # "" is an empty path that loads the index function, whos name is index
