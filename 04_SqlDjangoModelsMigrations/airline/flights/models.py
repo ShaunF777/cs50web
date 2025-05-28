7268750fb2f9f29bfcfc0006be58a930efa4815e
ShaunF777 <shaunfr777@gmail.com>
2025-05-28 14:24:53 +0200

    Refactor docstrings in Flight model for clarity and consistency

--- 04_SqlDjangoModelsMigrations/airline/flights/models.py
+++ 04_SqlDjangoModelsMigrations/airline/flights/models.py
@@ -5,15 +5,17 @@ class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField()
-''' My Django model is a class called Flight, where i have defined all the properties 
-    that a flight should have. Using Django syntax to find what type they should have.
-    The class inherits from models.Model, which is a base class provided by Django.
-    The properties are defined as fields with specific data types, such as CharField for strings
-    and IntegerField for integers. Each field corresponds to a column in the database table.
-'''
+    ''' My Django model is a class called Flight, where i have defined all the properties 
+        that a flight should have. Using Django syntax to find what type they should have.
+        The class inherits from models.Model, which is a base class provided by Django.
+        The properties are defined as fields with specific data types, such as CharField for strings
+        and IntegerField for integers. Each field corresponds to a column in the database table.
+    '''
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
-        ''' This method returns a string representation of the Flight object, which is useful for debugging and displaying flight information.
-            It formats the output to show the origin, destination, and duration of the flight.
-            The __str__ method is overridden to provide a human-readable representation of the Flight object.
-            This method is called when you print the object or convert it to a string. '''
+    ''' This method returns a string representation of the Flight object, which is useful for debugging and displaying flight information.
+        It formats the output to show the origin, destination, and duration of the flight.
+        The __str__ method is overridden to provide a human-readable representation of the Flight object.
+        This method is called when you print the object or convert it to a string. 
+    '''
+
