85d7b56cbe250e98a9d0878949191ec421c700f5
ShaunF777 <shaunfr777@gmail.com>
2025-05-28 14:35:16 +0200

    Change origin field in Flight model to ForeignKey referencing Airport model

--- 04_SqlDjangoModelsMigrations/airline/flights/models.py
+++ 04_SqlDjangoModelsMigrations/airline/flights/models.py
@@ -2,15 +2,19 @@ from django.db import models
 
 # Create your class(models) here. Each class will be a table in the database.
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
+    origin = models.ForeignKey('Airport', on_delete=models.CASCADE) 
+    ''' Reference to Airport model
+        indicating that each flight has an origin airport. if the airport is deleted, all flights
+        associated with that airport will also be deleted. This is a foreign key relationship.'''
+
+
     destination = models.CharField(max_length=64)
     duration = models.IntegerField()
     ''' My Django model is a class called Flight, where i have defined all the properties 
         that a flight should have. Using Django syntax to find what type they should have.
         The class inherits from models.Model, which is a base class provided by Django.
         The properties are defined as fields with specific data types, such as CharField for strings
-        and IntegerField for integers. Each field corresponds to a column in the database table.
-    '''
+        and IntegerField for integers. Each field corresponds to a column in the database table. '''
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
     ''' This method returns a string representation of the Flight object, which is useful for debugging and displaying flight information.
@@ -18,4 +22,9 @@ class Flight(models.Model):
         The __str__ method is overridden to provide a human-readable representation of the Flight object.
         This method is called when you print the object or convert it to a string. 
     '''
-
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    # This class defines an Airport model with two fields: code and city.
+    def __str__(self):
+        return f"{self.city} ({self.code})"
