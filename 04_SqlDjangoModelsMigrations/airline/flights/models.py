f7f02e3c281e60fdde0eef28c2c5d57ba2e4577d
ShaunF777 <shaunfr777@gmail.com>
2025-05-28 14:56:49 +0200

    Update Flight model to use ForeignKey for destination and enhance origin field documentation

--- 04_SqlDjangoModelsMigrations/airline/flights/models.py
+++ 04_SqlDjangoModelsMigrations/airline/flights/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your class(models) here. Each class will be a table in the database.
 class Flight(models.Model):
-    origin = models.ForeignKey('Airport', on_delete=models.CASCADE) 
-    ''' Reference to Airport model
-        indicating that each flight has an origin airport. if the airport is deleted, all flights
-        associated with that airport will also be deleted. This is a foreign key relationship.'''
+    origin = models.ForeignKey('Airport', on_delete=models.CASCADE, related_name='departures') 
+    ''' References to Airport model. Related name 'departures' allows access to all flights departing from a specific airport. 
+        If i query an airport, i can access all flights departing from that airport using the related name 'departures'. 
+        This is useful for querying flights based on their origin airport. The on_delete=models.CASCADE means that if an 
+        airport is deleted, all flights associated with that airport will also be deleted. This ensures referential integrity 
+        This is a foreign key relationship.'''
 
 
-    destination = models.CharField(max_length=64)
+    destination = models.ForeignKey('Airport', on_delete=models.CASCADE, related_name='arrivals')
+    ''' References to Airport model. Related name 'arrivals' allows access to all flights arriving at a specific airport.'''
     duration = models.IntegerField()
     ''' My Django model is a class called Flight, where i have defined all the properties 
         that a flight should have. Using Django syntax to find what type they should have.
