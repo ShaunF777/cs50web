5d23dd88714252a9064177129bdff4617313c45b
ShaunF777 <shaunfr777@gmail.com>
2025-05-30 20:19:37 +0200

    Add booking functionality to flight view and redirect after booking

--- 04_SqlDjangoModelsMigrations/airline/flights/views.py
+++ 04_SqlDjangoModelsMigrations/airline/flights/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Flight
 
 # Create your views here.
@@ -14,3 +16,10 @@ def flight(request, flight_id):
         "flight": flight , # Pass the flight object to the template
         "passengers": flight.passengers.all() # Fetch all passengers associated with the flight
     })
+
+def book(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id) # Get the flight with the given flight.ID
+    if request.method == "POST": # Check if the request method is POST
+        passenger = passenger.object.get(pk=int(request.POST["passenger"])) # Get the passenger from the primary key submitted via this form
+        passenger.flights.add(flight) # Add the flight to the passenger's flights
+        return HttpResponseRedirect(reverse('flight', args=(flight.id))) # Redirect to the flight page after booking
